49e34de38ecd71c7e55f7c0fec5957a0f46324b2
zhaopengfei <1027926875@qq.com>
2020-09-30 07:09:22 +0800

    gs consumeing rest

---
+++ gs-consuming-rest/src/main/java/com/erudev/consuming/Value.java
@@ -0,0 +1,37 @@
+package com.erudev.consuming;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * @author pengfei.zhao
+ * @date 2020/9/30 6:59
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Value {
+    private Long id;
+    private String quote;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    @Override
+    public String toString() {
+        return "Value{" +
+                "id=" + id +
+                ", quote='" + quote + '\'' +
+                '}';
+    }
+}
